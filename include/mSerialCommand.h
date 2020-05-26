09e13d87238fe1d35a02824db7b8b836a970960d
Valentin Py <valentin.py@gmail.com>
2020-05-26 12:20:21 +0200

    max command length: 12

--- include/mSerialCommand.h
+++ include/mSerialCommand.h
@@ -36,7 +36,7 @@
 // Size of the input buffer in bytes (maximum length of one command plus arguments)
 #define SERIALCOMMAND_BUFFER 32
 // Maximum length of a command excluding the terminating null
-#define SERIALCOMMAND_MAXCOMMANDLENGTH 8
+#define SERIALCOMMAND_MAXCOMMANDLENGTH 12
 
 // Uncomment the next line to run the library in debug mode (verbose messages)
 // #define SERIALCOMMAND_DEBUG
